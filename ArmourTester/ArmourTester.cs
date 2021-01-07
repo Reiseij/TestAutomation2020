e203a140d771c4d1bc5610c17ffd8219c641b903
Rasmus Savolainen <m1979@student.jamk.fi>
2021-01-07 13:59:19 +0200

    Lisätty yksikkotestiluokka ja sinne testiajureita

---
+++ ArmourTester/ArmourTester.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testing2018;
+
+namespace ArmourTester
+{
+    [TestClass]
+    public class ArmourTester
+    {
+        Armour ar = new Armour("Test Gloves", "Gloves for testing purposes", 10, 2, 1);
+
+        [TestMethod]
+        public void TestLevel()
+        {
+           
+            int lvl = ar.getLevel();
+            
+
+            // leveli voi olla vain 0 - 100
+            if (lvl < 0 || lvl > 100)
+            {
+                Assert.Fail();
+            }
+
+        }
+        [TestMethod]
+        public void TestSlot()
+        {
+
+            int slot = ar.getSlot();
+
+            // hanskojen slotti on aina 2
+            if (slot != 2)
+            {
+                Assert.Fail();
+            }
+
+        }
+        [TestMethod]
+        public void TestCondition()
+        {
+            string condition = ar.getCondition();
+
+            //Armorin kunto on alussa aina "Mint"
+            if (condition != "Mint")
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        public void TestDamagetaken1()
+        {
+            ar.takeDam(1);
+
+            //Armorin kunto damagen jälkeen ei voi olla täydellinen
+            if (ar.getCurProt() != ar.getMaxProt() - 1)
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        public void TestDamagetakenMax()
+        {
+            ar.takeDam(ar.getMaxProt());
+    
+            //Armorin kunto damagen max damagen jälkeen on 0
+            if (ar.getCurProt() != 0)
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        public void TestRepair1()
+        {
+            //Armorin kunto damagen max damagen jälkeen on 0
+            ar.takeDam(ar.getMaxProt());
+            ar.repair(1);
+            
+            if (ar.getCurProt() != 1)
+            {
+                Assert.Fail();
+            }
+        }
+        [TestMethod]
+        public void TestRepairMax()
+        {
+            //Armorin kunto damagen max damagen jälkeen on 0
+            ar.takeDam(ar.getMaxProt());
+
+            if (ar.getCurProt() != 0)
+            {
+                Assert.Fail();
+            }
+
+            ar.repair(ar.getMaxProt());
+
+            if (ar.getCurProt() != ar.getMaxProt())
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
---
+++ ArmourTester/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArmourTester")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArmourTester")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("589a32b9-3b60-46cf-ad52-57609d82d283")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
